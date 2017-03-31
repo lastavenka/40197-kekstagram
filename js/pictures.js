3563f008a102f69faa54ab459f7065e2f8fc76ed
Lastavenka <natasha.lastavenka@gmail.com>
2017-03-31 17:06:26 +0300

    Выполнено задание третьего раздела

--- js/pictures.js
+++ js/pictures.js
@@ -1,32 +1,26 @@
 'use strict';
 
-var COMMENTS = ['Всё отлично!',
-                'В целом всё неплохо. Но не всё.',
-                'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально.',
-                'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-                'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-                'Лица у людей на фотке перекошены, как-будто их избивают. Как можно было поймать такой неудачный момент?!'
-               ];
+var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как-будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var picturesNumber = 25;
 var likesMin = 15;
 var likesMax = 200;
 
 function getRandom(min, max) {
-    var rand = min + Math.random() * (max + 1 - min);
-    rand = Math.floor(rand);
-    return rand;
+  var rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
 }
 
 var getPictures = function (arrSize) {
   var picturesList = [];
-    
+
   for (var i = 0; i < arrSize; i++) {
     picturesList[i] = {
-      url: 'photos/' + (i+1) + '.jpg',
+      url: 'photos/' + (i + 1) + '.jpg',
       likes: getRandom(likesMin, likesMax),
       comments: COMMENTS[getRandom(0, COMMENTS.length - 1)]
     };
-  };
+  }
   return picturesList;
 };
 
@@ -34,7 +28,7 @@ var getPictures = function (arrSize) {
 var pictureTemplate = document.querySelector('#picture-template').content;
 var renderPicture = function (picture) {
   var pictureElement = pictureTemplate.cloneNode(true);
-  
+
   pictureElement.getElementsByTagName('img').src = picture.url;
   pictureElement.querySelector('.picture-likes').textContent = picture.likes;
   pictureElement.querySelector('.picture-comments').textContent = picture.comments;
